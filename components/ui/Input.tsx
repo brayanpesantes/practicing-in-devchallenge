c7aa89689f4f409f22370919a5eeacff53994a0a
banp95 <alexisdev95@gmail.com>
2024-04-02 10:11:02 -0500

    install react-form-hook and update componentes for validation

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,16 +1,41 @@
 import cn from "@/utils/cn";
 import { InputHTMLAttributes } from "react";
+import { useController } from "react-hook-form";
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  control: any;
+  rules?: any;
+}
 
-export const Input = ({ className, ...rest }: Props) => {
+export const Input = ({ className, name, control, rules, ...rest }: Props) => {
+  const {
+    field: { ref, ...fields },
+    fieldState: { invalid, isTouched, isDirty, error },
+  } = useController({
+    name,
+    control,
+    rules,
+  });
   return (
-    <input
-      className={cn([
-        "ps-3 py-6 rounded-xl text-base font-medium text-[#111729] w-full  placeholder:text-[#CDD5E0]",
-        className,
-      ])}
-      {...rest}
-    />
+    <div className="">
+      <input
+        className={cn([
+          "ps-3 py-6 rounded-xl text-base font-medium text-[#111729] w-full placeholder:text-[#CDD5E0]",
+          className,
+          {
+            "border-red-400 focus:border-none focus:ring-2 focus:ring-red-400":
+              invalid && isTouched,
+          },
+        ])}
+        {...rest}
+        {...fields}
+        ref={ref}
+      />
+
+      {isTouched && invalid && (
+        <div className="text-red-400">{error?.message}</div>
+      )}
+    </div>
   );
 };
--- components/ui/Select.tsx
+++ components/ui/Select.tsx
@@ -1,5 +1,7 @@
-'use client'
+"use client";
+import cn from "@/utils/cn";
 import React, { SelectHTMLAttributes } from "react";
+import { useController } from "react-hook-form";
 
 type Option = {
   label: string;
@@ -7,21 +9,52 @@ type Option = {
 };
 
 interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  control: any;
+  rules?: any;
   options: Option[];
 }
 
-export const Select = ({ options, ...rest }: Props) => {
+export const Select = ({
+  className,
+  name,
+  control,
+  rules,
+  options,
+  ...rest
+}: Props) => {
+  const {
+    field: { ref, ...fields },
+    fieldState: { invalid, isTouched, isDirty, error },
+  } = useController({
+    name,
+    control,
+    rules,
+  });
+
   return (
-    <select
-      name="company-size"
-      id="company-size"
-      className="ps-3 py-6 w-full rounded-lg text-[#111729] font-medium form-select "
-    >
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
+    <div>
+      <select
+        id="company-size"
+        className={cn([
+          "ps-3 py-6 w-full rounded-lg text-[#111729] font-medium form-select",
+          className,
+          { "border-red-400": isTouched && invalid },
+        ])}
+        {...rest}
+        {...fields}
+        ref={ref}
+      >
+        <option value="">Seleccione una opción ...</option>
+        {options.map((option, index) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {isTouched && invalid && (
+        <div className="text-red-400">{error?.message}</div>
+      )}
+    </div>
   );
 };
