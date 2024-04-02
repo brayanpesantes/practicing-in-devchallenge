0da80ee9a98f37f163cf87c133107f8cadaaf5c2
banp95 <alexisdev95@gmail.com>
2024-04-02 10:11:28 -0500

    add validation a inputs

--- app/contact/_components/CardForm.tsx
+++ app/contact/_components/CardForm.tsx
@@ -1,22 +1,56 @@
+"use client";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 import { Label } from "@/components/ui/Label";
 import { Select } from "@/components/ui/Select";
+import { Span } from "next/dist/trace";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-type Props = {};
+type Inputs = {
+  name: string;
+  "company-email": string;
+  "company-size": string;
+  subject: string;
+  message: string;
+};
 
-export const CardForm = (props: Props) => {
+export const CardForm = () => {
+  const { control, handleSubmit, register } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => alert(JSON.stringify(data));
+
   return (
     <div className="w-full xl:w-[810px] xl:mx-auto pt-10">
-      <div className="p-4 md:p-8 mx-6 lg:mx-8 xl:mx-auto flex flex-col gap-6 bg-[#CDD5E0]/20 rounded-xl">
+      <form
+        className="p-4 md:p-8 mx-6 lg:mx-8 xl:mx-auto flex flex-col gap-6 bg-[#CDD5E0]/20 rounded-xl"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="flex flex-col lg:flex-row gap-[18px] w-full ">
           <div className="w-full">
             <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="Ethan Johnson" />
+            <Input
+              id="name"
+              name="name"
+              control={control}
+              rules={{ required: "Este campa es obligatorio" }}
+              placeholder="Ethan Johnson"
+            />
           </div>
           <div className="w-full">
-            <Label htmlFor="name">Company Email</Label>
-            <Input id="name" placeholder="ethan@johnson.com" />
+            <Label htmlFor="company-email">Company Email</Label>
+            <Input
+              id="company-email"
+              placeholder="ethan@johnson.com"
+              name="company-email"
+              control={control}
+              rules={{
+                required: "El correo electrónico es obligatorio",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Correo electrónico inválido",
+                },
+              }}
+            />
           </div>
         </div>
         <div className="flex flex-col lg:flex-row gap-[18px] w-full">
@@ -25,6 +59,8 @@ export const CardForm = (props: Props) => {
             <Select
               id="company-size"
               name="company-size"
+              control={control}
+              rules={{ required: "debe de seleccionar una opción" }}
               options={[
                 { label: "50-100 employees", value: "50-100" },
                 { label: "101-150 employees", value: "101-150" },
@@ -33,8 +69,12 @@ export const CardForm = (props: Props) => {
             />
           </div>
           <div className="w-full">
-            <Label htmlFor="Subject">Subject</Label>
+            <Label htmlFor="subject">Subject</Label>
             <Select
+              id="subject"
+              name="subject"
+              control={control}
+              rules={{ required: "debe de seleccionar una opción" }}
               options={[
                 {
                   label: "Building Landing pages",
@@ -50,13 +90,13 @@ export const CardForm = (props: Props) => {
           <Label>Message</Label>
           <textarea
             id="message"
-            name="message"
+            {...register("message")}
             className="mt-1 w-full ps-3 resize-none rounded-xl h-52 placeholder:text-[#111729]"
             placeholder="50-100 employees"
           ></textarea>
         </div>
-        <Button>Contact Sales</Button>
-      </div>
+        <Button type="submit">Contact Sales</Button>
+      </form>
     </div>
   );
 };
