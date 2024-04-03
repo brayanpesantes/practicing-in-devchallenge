62e4883fe51fbabf2641f796f44f0dcd01b25ae3
brayanpesantes <brayalexis.30@gmail.com>
2024-04-03 18:22:06 -0500

    create icons

---
+++ app/music-player/_components/IconPause.tsx
@@ -0,0 +1,15 @@
+import { SVGProps } from "react";
+
+export function IconPause(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path fill="currentColor" d="M6 19h4V5H6zm8-14v14h4V5z"></path>
+    </svg>
+  );
+}
