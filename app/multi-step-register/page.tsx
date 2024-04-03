fcbe381c0cc07d91e726efc30a970a2e41011a17
brayanpesantes <brayalexis.30@gmail.com>
2024-04-03 13:01:50 -0500

    update phone to for rotate

--- app/multi-step-register/page.tsx
+++ app/multi-step-register/page.tsx
@@ -5,7 +5,7 @@ import Steps from "./_components/Steps";
 export default function MultiStepRegisterPage() {
   return (
     <FormStepProvider>
-      <div className="relative h-screen max-w-screen-xl bg-left mx-auto bg-[#121826] bg-no-repeat overflow-hidden font-inter">
+      <div className="relative min-h-screen max-w-screen-xl bg-left mx-auto bg-[#121826] bg-no-repeat overflow-hidden font-inter">
         <div className="absolute bg-[url('/images/blur-radial.svg')] bg-no-repeat -left-[400px] -top-[400px]  size-[800px] bg-cover  z-10"></div>
 
         <div className="absolute bg-[url('/images/blur-radial.svg')] bg-no-repeat -bottom-[400px] -right-[400px] size-[800px] bg-cover z-10 "></div>
