aa0a29bc297f0ba0b3b2b30d42d2decc5ee39c6e
brayanpesantes <brayalexis.30@gmail.com>
2024-04-03 12:42:39 -0500

    update the data to see the steps

--- app/multi-step-register/page.tsx
+++ app/multi-step-register/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { FormStepProvider } from "@/context/FormStepContext";
-import DataSummary from "./_components/DataSummary";
-// import PersonalInfo from "./_components/PersonalInfo";
-// import TopicInterested from "./_components/TopicInterested";
+import Steps from "./_components/Steps";
 
 export default function MultiStepRegisterPage() {
   return (
@@ -13,10 +11,8 @@ export default function MultiStepRegisterPage() {
         <div className="absolute bg-[url('/images/blur-radial.svg')] bg-no-repeat -bottom-[400px] -right-[400px] size-[800px] bg-cover z-10 "></div>
 
         <div className="absolute inset-0 z-20">
-          <div className="flex items-center justify-center w-full h-full p-3 md:p-0">
-            {/* <PersonalInfo /> */}
-            {/* <TopicInterested /> */}
-            <DataSummary />
+          <div className="flex flex-col items-center justify-center w-full h-full p-3 md:p-0">
+            <Steps />
           </div>
         </div>
       </div>
