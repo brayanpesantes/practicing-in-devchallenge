d4eea574fd7180df0f4cb79f9df9c7965e2dcb71
brayanpesantes <brayalexis.30@gmail.com>
2024-04-03 12:40:11 -0500

    add context the steps

--- context/FormStepContext.tsx
+++ context/FormStepContext.tsx
@@ -24,6 +24,9 @@ interface FormData {
 interface FormStepContextType {
   formData: FormData;
   handleChange: (data: any, stepName: string) => void;
+  MAX_STEPS: number;
+  goToNextStep: () => void;
+  currentStep: number;
 }
 
 const initialFormData: FormData = {
@@ -39,12 +42,18 @@ const initialFormData: FormData = {
 export const FormStepContext = createContext<FormStepContextType>({
   formData: initialFormData,
   handleChange: () => {},
+  MAX_STEPS: 3,
+  goToNextStep: () => {},
+  currentStep: 1,
 });
 
 export const FormStepProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [formData, setFormData] = useState<FormData>(initialFormData);
+  const [currentStep, setCurrentStep] = useState<number>(1);
+
+  const MAX_STEPS = 3;
 
   const handleChange = useCallback(
     (data: any, stepName: string) => {
@@ -55,11 +64,19 @@ export const FormStepProvider: React.FC<{ children: ReactNode }> = ({
     },
     [setFormData]
   );
+  const goToNextStep = useCallback(() => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === MAX_STEPS) {
+        return 1;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  }, []);
 
   const value: FormStepContextType = useMemo(() => {
-    return { formData, handleChange };
-  }, [formData, handleChange]);
-
+    return { formData, handleChange, MAX_STEPS, goToNextStep, currentStep };
+  }, [formData, handleChange, currentStep, goToNextStep, MAX_STEPS]);
   return (
     <FormStepContext.Provider value={value}>
       {children}
