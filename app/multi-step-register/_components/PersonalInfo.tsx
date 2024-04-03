dd5658d063a5708ebc667f37e6f79b0d913154fe
banp95 <alexisdev95@gmail.com>
2024-04-03 01:26:59 -0500

    update component personal info

--- app/multi-step-register/_components/PersonalInfo.tsx
+++ app/multi-step-register/_components/PersonalInfo.tsx
@@ -3,6 +3,8 @@ import { Input } from "@/components/ui/Input";
 import { Label } from "@/components/ui/Label";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Card from "./Card";
+import { Button } from "@/components/ui/Button";
+import { useFormStepContext } from "@/context/FormStepContext";
 
 type Inputs = {
   name: string;
@@ -11,7 +13,12 @@ type Inputs = {
 
 export default function PersonalInfo() {
   const { control, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => alert(JSON.stringify(data));
+
+  const { formData, handleChange } = useFormStepContext();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    handleChange(data, "personalInfo");
+  };
 
   return (
     <Card>
@@ -22,7 +29,9 @@ export default function PersonalInfo() {
           <Input
             name="name"
             placeholder="name"
+            value={formData.personalInfo.name}
             control={control}
+            rules={{ required: "Name is required" }}
             className="py-4 border outline-none  border-[#A1A1A9] bg-[#212936] focus:ring-1 focus:ring-[#652CD1] focus:outline-none caret-white text-[#E5E7EB] placeholder:text-[#A1A1A9]"
           />
         </div>
@@ -31,12 +40,22 @@ export default function PersonalInfo() {
           <Input
             name="email"
             control={control}
+            value={formData.personalInfo.email}
+            rules={{
+              required: "Email is Requerid",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Email is invalided",
+              },
+            }}
             className="py-4 border border-[#A1A1A9] bg-[#212936] focus:ring-1 focus:ring-[#652CD1] focus:outline-none caret-white text-[#E5E7EB] placeholder:text-[#A1A1A9]"
             placeholder="example@gmail.com"
           />
         </div>
-        <div className="mt-9">
-          <button>Coutinue</button>
+        <div className="mt-9 flex items-center justify-center">
+          <Button className="px-7 py-3 rounded-full bg-[#845EEE] text-white hover:bg-[#652CD1]">
+            Continue
+          </Button>
         </div>
       </form>
     </Card>
