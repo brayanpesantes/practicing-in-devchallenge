3bdaed745800eefc1ad7531336d4105607d84863
banp95 <alexisdev95@gmail.com>
2024-04-12 20:43:38 -0500

    create hero

---
+++ app/country/components/Hero.tsx
@@ -0,0 +1,9 @@
+import Logo from "./Logo";
+
+export default function Hero() {
+  return (
+    <div className="h-[300px] flex justify-center bg-country bg-center bg-no-repeat bg-cover">
+      <Logo />
+    </div>
+  );
+}
