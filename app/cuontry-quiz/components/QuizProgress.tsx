a792a63d300b33e687786573c26231a701205632
banp95 <alexisdev95@gmail.com>
2024-04-13 16:54:37 -0500

    upgraded progress separe a component

---
+++ app/cuontry-quiz/components/QuizProgress.tsx
@@ -0,0 +1,32 @@
+import cn from "@/utils/cn";
+
+interface QuizProgressProps {
+  readonly currentIndex: number;
+  readonly totalQuestions: number;
+}
+export default function QuizProgress({
+  currentIndex,
+  totalQuestions,
+}: QuizProgressProps) {
+  return (
+    <div className="flex flex-row gap-x-3 mt-4">
+      {Array.from({ length: totalQuestions }, (_, i) => (
+        <div
+          key={i}
+          className={cn(
+            "size-11 rounded-full bg-[#393F6E] text-[#E2E4F3] flex items-center justify-center text-sm font-bold",
+            {
+              "bg-gradient-to-r from-[#E65895] to-[#BC6BE8]":
+                i === currentIndex,
+            },
+            {
+              "bg-gradient-to-r from-[#E65895] to-[#BC6BE8]": i < currentIndex,
+            }
+          )}
+        >
+          {i + 1}
+        </div>
+      ))}
+    </div>
+  );
+}
