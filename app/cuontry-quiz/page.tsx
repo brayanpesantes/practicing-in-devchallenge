f06a752064fee227f39a1056fd039224d2dab5b0
brayanpesantes <brayalexis.30@gmail.com>
2024-04-13 14:49:41 -0500

    create page de country quiz

---
+++ app/cuontry-quiz/page.tsx
@@ -0,0 +1,9 @@
+import Content from "./components/Content";
+
+export default function page() {
+  return (
+    <div className="max-w-screen-xl min-h-screen mx-auto bg-quiz bg-cover bg-no-repeat bg-center font-beVietnamPro">
+      <Content />
+    </div>
+  );
+}
