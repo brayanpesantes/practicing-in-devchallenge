47a720d300d09f1f88eb19ce7e5a2748b203c91a
brayanpesantes <brayalexis.30@gmail.com>
2024-04-16 12:09:03 -0500

    created page home the Task

---
+++ app/task-board/page.tsx
@@ -0,0 +1,11 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
+export default function TaskBoardPage() {
+  return (
+    <div className="max-w-screen-xl h-screen bg-[#F8FAFC] mx-auto font-outfit relative">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
