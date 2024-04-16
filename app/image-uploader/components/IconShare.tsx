463d30700bf12435a2701b0d516d119f56694f7c
banp95 <alexisdev95@gmail.com>
2024-04-16 08:05:40 -0500

    created icons

---
+++ app/image-uploader/components/IconShare.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+export function IconShare(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m13.12 17.023l-4.199-2.29a4 4 0 1 1 0-5.465l4.2-2.29a4 4 0 1 1 .958 1.755l-4.2 2.29a4.008 4.008 0 0 1 0 1.954l4.2 2.29a4 4 0 1 1-.959 1.755M6 14a2 2 0 1 0 0-4a2 2 0 0 0 0 4m11-6a2 2 0 1 0 0-4a2 2 0 0 0 0 4m0 12a2 2 0 1 0 0-4a2 2 0 0 0 0 4"
+      ></path>
+    </svg>
+  );
+}
