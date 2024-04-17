184f626d2b62898ee5a5b301801e5330846e07ae
brayanpesantes <brayalexis.30@gmail.com>
2024-04-16 19:36:04 -0500

    upgrade logic

--- app/task-board/page.tsx
+++ app/task-board/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 export default function TaskBoardPage() {
   return (
-    <div className="max-w-screen-xl h-screen bg-[#F8FAFC] mx-auto font-outfit relative">
+    <div className="max-w-screen-xl h-screen bg-[#F8FAFC] mx-auto font-outfit relative overflow-hidden ">
       <Navbar />
       <Main />
     </div>
