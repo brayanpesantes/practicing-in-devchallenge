13f215acabdc56eb6a66d6667a5be8ea19887a69
brayanpesantes <brayalexis.30@gmail.com>
2024-04-18 11:06:29 -0500

    update class color and event edit

--- app/task-board/components/Task.tsx
+++ app/task-board/components/Task.tsx
@@ -1,24 +1,31 @@
-import { Task as TypeTask } from "@/types/task";
+import { useTaskContext } from "@/context/TasksContext";
+import { Status, Task as TypeTask } from "@/types/task";
 import Card from "./Card";
 import IconState from "./IconState";
 import IconTypeCard from "./IconTypeCard";
-import { Status } from "./TaskForm";
 type TaskProps = {
   readonly task: TypeTask;
-  readonly open: () => void;
 };
-export default function Task({ task, open }: TaskProps) {
+
+const statusColorMap: { [key: string]: string } = {
+  [Status.COMPLETED]: "bg-[#A0ECB1]",
+  [Status.DEFAULT]: "bg-[#97A3B6]",
+  [Status.IN_PROGRESS]: "bg-[#F5D565]",
+  [Status.WONT_DO]: "bg-[#F7D4D3]",
+};
+
+export default function Task({ task }: TaskProps) {
+  const { openModalForEdit } = useTaskContext();
+
+  const color = statusColorMap.hasOwnProperty(task.status)
+    ? statusColorMap[task?.status]
+    : "";
+
   return (
-    <Card
-      color={`${task.type === Status.COMPLETED && "bg-[#A0ECB1]"} 
-      ${task.type === Status.DEFAULT && "bg-[#97A3B6]"}
-       ${task.type === Status.IN_PROGRESS && "bg-[#F5D565]"}
-       ${task.type === Status.WONT_DO && "bg-[#F7D4D3]"}
-       `}
-    >
+    <Card color={color}>
       <button
         className="flex w-full flex-row items-center gap-5 cursor-pointer"
-        onClick={open}
+        onClick={() => openModalForEdit(task)}
       >
         <div className="self-start">
           <IconTypeCard icon={task.icon} />
@@ -31,7 +38,7 @@ export default function Task({ task, open }: TaskProps) {
             )}
           </div>
           <div className="">
-            <IconState icon={task.type} />
+            <IconState icon={task.status} />
           </div>
         </div>
       </button>
