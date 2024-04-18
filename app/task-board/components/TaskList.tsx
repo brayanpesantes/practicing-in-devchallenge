f783dc8e2dad498566a0900ecad8b68771262785
brayanpesantes <brayalexis.30@gmail.com>
2024-04-18 11:08:33 -0500

    update list add scroll

--- app/task-board/components/TaskList.tsx
+++ app/task-board/components/TaskList.tsx
@@ -6,12 +6,14 @@ import Task from "./Task";
 
 export default function TaskList() {
   const { tasks, openModal } = useTaskContext();
-  return (
-    <div className="flex flex-col gap-y-5">
-      {tasks?.map((task) => (
-        <Task key={task.id} task={task} open={open} />
-      ))}
 
+  return (
+    <div className="flex flex-col gap-y-5 h-[700px] overflow-hidden">
+      <div className="flex flex-col gap-5 overflow-auto scroll-smooth ">
+        {tasks?.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </div>
       <Card color="bg-[#F5E8D5]">
         <button
           className="flex flex-row items-center gap-5 w-full h-full"
