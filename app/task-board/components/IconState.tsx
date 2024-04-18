fade329657a178075826cb5e5316d6ee851b42b2
brayanpesantes <brayalexis.30@gmail.com>
2024-04-18 11:03:52 -0500

    refractory componente and logic

--- app/task-board/components/IconState.tsx
+++ app/task-board/components/IconState.tsx
@@ -1,35 +1,30 @@
+import { Status } from "@/types/task";
 import cn from "@/utils/cn";
 import IconCloseRing from "./IconCloseRing";
 import IconDone from "./IconDone";
 import IconTimeAtack from "./IconTimeAtack";
-import { Status } from "./TaskForm";
+
 type IconStateProps = {
   readonly icon?: string;
 };
 
+const iconMap: { [key: string]: JSX.Element } = {
+  [Status.COMPLETED]: <IconDone />,
+  [Status.IN_PROGRESS]: <IconTimeAtack />,
+  [Status.WONT_DO]: <IconCloseRing />,
+};
+
+const colorMap: { [key: string]: string } = {
+  [Status.COMPLETED]: "bg-[#32D657]",
+  [Status.IN_PROGRESS]: "bg-[#E9A23B]",
+  [Status.WONT_DO]: "bg-[#DD524C]",
+};
+
 export default function IconState({ icon = "completed" }: IconStateProps) {
-  const typeIcons: { [key: string]: JSX.Element } = {
-    COMPLETED: <IconDone />,
-    IN_PROGRESS: <IconTimeAtack />,
-    WONT_DO: <IconCloseRing />,
-  };
+  const iconComponent = iconMap[icon] || null;
+  const backgroundColor = colorMap[icon] || "";
 
   return (
-    <div
-      className={cn(
-        "p-3 rounded-lg",
-        {
-          "bg-[#32D657]": icon === Status.COMPLETED,
-        },
-        {
-          "bg-[#E9A23B]": icon === Status.IN_PROGRESS,
-        },
-        {
-          "bg-[#DD524C]": icon === Status.WONT_DO,
-        }
-      )}
-    >
-      {typeIcons[icon]}
-    </div>
+    <div className={cn("p-3 rounded-lg", backgroundColor)}>{iconComponent}</div>
   );
 }
