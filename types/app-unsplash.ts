312e2cdf84ab4bd45ef0fb204232ee13256ddac8
brayanpesantes <brayalexis.30@gmail.com>
2024-04-19 13:38:23 -0500

    created types

---
+++ types/app-unsplash.ts
@@ -0,0 +1,12 @@
+export type Photo = {
+  id: string;
+  description: string;
+  width: number;
+  height: number;
+  urls: { large: string; regular: string; raw: string; small: string };
+  color: string | null;
+  user: {
+    username: string;
+    name: string;
+  };
+};
