67d40565c1d867919ed15936e7f9d8ba78940566
brayanpesantes <brayalexis.30@gmail.com>
2024-04-23 11:52:07 -0500

    update add list Photo

--- app/unsplash-collection/collections/photo/[id]/page.tsx
+++ app/unsplash-collection/collections/photo/[id]/page.tsx
@@ -1,34 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
-import { Photo } from "@/types/app-unsplash";
-import { api } from "@/utils/unsplasn";
+import PhotoList from "@/app/unsplash-collection/components/PhotoList";
 import { useParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
 
 export default function PhotoIdPage() {
   const params = useParams();
-  const [photos, setPhotos] = useState<Photo[] | null>(null);
-  const getCollection = () => {
-    api.collections
-      .getPhotos({ collectionId: String(params?.id), page: 1, perPage: 30 })
-      .then((res) => {
-        if (res.type === "success") {
-          setPhotos(res.response.results);
-        }
-      });
-  };
-
-  useEffect(() => {
-    getCollection();
-  }, []);
-
-  return (
-    <div className="grid grid-cols-4 gap-6">
-      {photos?.map((photo) => (
-        <div key={photo.id}>
-          <img src={photo.urls.regular} alt={photo.id} />
-        </div>
-      ))}
-    </div>
-  );
+  return <PhotoList id={String(params?.id)} />;
 }
