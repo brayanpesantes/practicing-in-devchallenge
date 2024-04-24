f69f557fbc1f2678cbbd5e12bbcdb67d5496b3a8
banp95 <alexisdev95@gmail.com>
2024-04-23 19:33:43 -0500

    updating list

--- app/page.tsx
+++ app/page.tsx
@@ -26,19 +26,21 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="mt-10">
+    <main className="mt-10 max-w-screen-xl mx-auto  lg:px-20">
       <h1 className="text-5xl text-center">
         PROJECTS DE
         <Link href="https://devchallenges.io/" target="_blank">
           <strong> devchallenges</strong>
         </Link>
       </h1>
-      <div className="grid grid-cols-4 max-w-screen-2xl mx-auto py-10 gap-10">
+      <div className="grid grid-cols-1 md:lg:grid-cols-2 lg:grid-cols-3  py-10 gap-10">
         {projects.map((project) => (
           <Card
             key={project.id}
             image={project.image_url}
             title={project.name}
+            github_url={project.github_url}
+            slug={project.slug}
           />
         ))}
       </div>
